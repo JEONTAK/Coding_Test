68cd8c4f1119811fdcadfd770686e5a9090b47e3
JEONTAK <tak980418@naver.com>
2025-01-03 10:32:10 +0900

    2025.01.03 BaekJoon : 앱 (Gold 3)

--- src/BaekJoon/Gold3/앱/BOJ7579.java
+++ src/BaekJoon/Gold3/앱/BOJ7579.java
@@ -7,35 +7,34 @@ import java.util.StringTokenizer;
 
 public class BOJ7579 {
 
-    static int N, M, maxCost = 0;
-    static int[][] app;
-    static int[] dp;
-
     private static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        app = new int[N][2];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] app = new int[N][2];
+
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             app[i][0] = Integer.parseInt(st.nextToken());
         }
+
+        int cost = 0;
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             app[i][1] = Integer.parseInt(st.nextToken());
-            maxCost += app[i][1];
+            cost += app[i][1];
         }
 
-        dp = new int[maxCost + 1];
+        int[] dp = new int[cost + 1];
 
         for (int i = 0; i < N; i++) {
-            for (int j = maxCost; j >= app[i][1]; j--) {
+            for (int j = cost; j >= app[i][1]; j--) {
                 dp[j] = Math.max(dp[j], dp[j - app[i][1]] + app[i][0]);
             }
         }
 
-        for (int i = 0; i <= maxCost; i++) {
+        for (int i = 0; i <= cost; i++) {
             if (dp[i] >= M) {
                 System.out.println(i);
                 return;
@@ -51,32 +50,28 @@ public class BOJ7579 {
 /*
 문제 분석
 1. 정보
-    - N개의 앱이 활성화 되어있음 (A_1, ..., A_N)
-        - 각 앱 A_i
-            - m_i : 메모리 사용량
-            - c_i : 비활성화 이후 다시 실행할 시 추가적인 비용
-    - 새로운 앱 B를 실행하고자 하기 위해 필요한 메모리의 양 : M
-        - 즉 A_1, ... ,A_N 중에서 몇 개를 비활성화 하여 M 바이트 이상의 메모리를 확보 해야함
-        - 이 때의 비용 c_i의 합을 최소화 해야함.
-2. 목표
-    - 필요한 메모리 M 바이트를 확보하기 위한 앱 비활성화의 최소의 비용
+    - N개의 앱, A_1, ..., A_N 이 활성화 되어 있다고 가정
+    - 앱 A_i 는 각각 m_i 바이트 만큼의 메모리를 사용중
+    - 앱 A_i 를 비활성화 한 후에 다시 실행하고자 할 경우, 추가 비용이 c_i 만큼 들음
+    - M 바이트의 추가 메모리가 필요함
 
+2. 목표
+    - 필요한 메모리 M 바이트를 확보하기 위한 앱 비활성화의 최소 비용을 출력
 3. 제약 조건
-    - 1 <= N <= 100
-    - 1 <= M <= 10,000,000
-    - 1 <= m_1, ..., m_N <= 10,000,000
-    - 1 <= c_1, ..., c_N <= 100
-    - M <= m_1 + m_2 + ... + m_N
+     - N : 1 <= N <= 100
+     - M : 1 <= M <= 10_000_000
+     - 1 <= m_1, ..., m_N <= 10_000_000
+     - 0 <= c_1, ..., c_N <= 100
+     - M <= m_1 + m_2 + ... + m_N
 
 풀이
 1. 알고리즘
-    - DP
-        - DP[i] : 비용이 i일때 확보할수 있는 최대 메모리
-        - 0 ~ N까지의 앱(i)
-            - 전체 cost 총합 ~ c_i 까지(j)
-            - DP[j] = DP[j - c_i] + m_i의 값들 중 최대값
-            - 비용 j일때의 최대 메모리 = (j - 현재 App의 비용)일때의 최대 메모리 + 현재 앱의 메모리 의 최대 값
-
-    - 이후 해당 DP 배열 0부터 maxCost까지
-        - 만약 DP[i] >= M -> i의 비용으로 메모리 확보 가능 -> i가 정답
- */
+    - app[N][2] : app[N][0] : 메모리 / app[N][1] : 비용
+    - DP 알고리즘
+        - DP[i] : i 비용을 썼을 때, 비 활성화 할 수 있는 메모리의 최댓값
+        - DP 범위 : 0 ~ 비용의 합(cost)
+            - 앱 i : 0 ~ N
+            - 비용 j : cost ~ app[i]의 비용까지
+                - DP[j] : j 비용을 썼을 때의 최대 메모리 값, app[i]를 비활성화를 하여 j 비용이 되었을 때의 최대 메모리값을 비교하여 최댓값 저장
+                - DP[j] = Math.max(DP[j], DP[j - app[i][1] + app[i][0]);
+*/
